b1ebce448ff314820ba1bb5cf60746e9806bebd4
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2024-04-27 02:06:27 +0300

    new files

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -17,48 +17,6 @@ import {Image} from './ImageGalleryItem.styled'
 
 import {Modal} from '../Modal/Modal'
 
-
-// export class ImageGalleryItem extends Component {
-//   state = {
-//     isModalOpen: false,
-//   };
-
-//   openModal = () => {
-//     this.setState({ isModalOpen: true });
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   };
-
-//   closeModal = () => {
-//     this.setState({ isModalOpen: false });
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   };
-
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.closeModal();
-//     }
-//   };
-
-//   render() {
-//     const { webformatURL, tags} = this.props.image;
-//     const { isModalOpen } = this.state;
-
-//     return (
-//       <Image className="gallery-item">
-//         <img
-//           src={webformatURL}
-//           alt={tags}
-//           onClick={this.openModal}
-//           className="gallery-image"
-//         />
-//         {isModalOpen && (
-//           <Modal onClose={this.closeModal} image={this.props.image} />
-//         )}
-//       </Image>
-//     );
-//   }
-// }
-
 export const ImageGalleryItem = ({image}) =>{
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { webformatURL, tags, largeImageURL} = image;
